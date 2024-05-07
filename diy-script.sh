cf6b0dc8dcd5bb8dc17f5fe48ffc41517b49f119
自宅警备员 <61686557+lsbst1986@users.noreply.github.com>
2024-05-07 11:25:53 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # 修改默认IP
-# sed -i 's/192.168.0.55/192.168.1.1/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.1.253/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.88.3/g' package/base-files/files/bin/config_generate
 sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' target/linux/sunxi/Makefile
-sed -i 's/OpenWrt/SunxiA53/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/SunxiCortexA53/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
