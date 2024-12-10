f6f97e87a7cd43fc6724fb050331648ea563f78b
自宅警备员 <61686557+lsbst1986@users.noreply.github.com>
2024-12-10 10:37:05 +0800

    Update diy-script_x86.sh

--- diy-script_x86.sh
+++ diy-script_x86.sh
@@ -29,7 +29,7 @@ git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/lu
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
 
 # 科学上网插件
-git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
+git clone --depth=1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 #git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
