058111888f91189db15efeaf2213c1ee32180aac
自宅警备员 <61686557+lsbst1986@users.noreply.github.com>
2025-02-08 11:43:19 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -30,7 +30,8 @@ git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/lu
 
 # 科学上网插件
 rm -rf package/helloworld
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+#git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone --depth=1 -b main https://github.com/sbwml/openwrt_helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 #git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
@@ -38,7 +39,7 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-a
 # Themes
 #git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 #git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
 #git_sparse_clone main https://github.com/haiibo/packages luci-theme-atmaterial luci-theme-opentomcat luci-theme-netgear
 
